c121b0b875ca61391ab8554f3057c9bf64076545
John Evans <evanjo03@luther.edu>
2019-04-17 17:30:12 -0500

    new db

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var exphbs = require("express-handlebars");
 var db = require("./models");
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mydb";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
